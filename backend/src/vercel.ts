e499890fa9cc368b0106a12d2055f70a30c52a2b
tarekbalosh <tarekba850@gmail.com>
2026-04-24 23:55:56 +0800

    fix: route /api to serverless NestJS handler on Vercel

---
+++ backend/src/vercel.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+
+let cachedApp: any;
+
+export default async function handler(req: any, res: any) {
+  if (!cachedApp) {
+    cachedApp = await NestFactory.create(AppModule);
+    cachedApp.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+    cachedApp.use(helmet({
+      crossOriginResourcePolicy: false,
+    }));
+    cachedApp.enableCors({
+      origin: true,
+      credentials: true,
+    });
+    await cachedApp.init();
+  }
+  
+  const instance = cachedApp.getHttpAdapter().getInstance();
+  return instance(req, res);
+}
